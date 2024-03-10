9cd9bd4209521dead1fe3aa1718794a4cb7dce1d
Ruwan <ruwanliyanagegalle1994@gmail.com>
2024-03-10 12:43:08 +0530

    Modify the router

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -1,32 +1,40 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import MainLayout from '../Layout/MainLayout'
 import Home from '../Pages/Home/Home'
 import User from '../Pages/User/User'
 import Login from '../Pages/Login/Login'
 import UserProtected from './UserProtected'
 
+//after versoin 6
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout />,
+        children: [
+            {
+                index: true,
+                element: <Home/>
+            },
+            {
+               element:<UserProtected />,
+               children: [
+                {
+                    path: 'user',
+                    element: <User />
+                }
+               ]
+            }
+        ]
+    },
+    {
+        path: '/login',
+        element: <Login/>
+    }
+]);
+
 const AppRouter = () => {
-  return (
-    // before version 6 related routing
-    <BrowserRouter>
-        <Routes >
-            <Route path="/" element={<MainLayout />}>
-                <Route index element={<Home />} />
-                <Route element={ <UserProtected />}>
-                    <Route path='user' element={<User />}/>
-                </Route>
-                {/* 
-                    index - path location of the outer       
-                    outlet- inside elements of the outer
-                */}
-            </Route>
-            <Route path='/login'>
-               <Route index element={<Login/>}/>
-            </Route>
-        </Routes>
-    </BrowserRouter>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default AppRouter
