fc3196694af0a00eda777d8a5f61d6eb1f8a71b4
Ruwan <ruwanliyanagegalle1994@gmail.com>
2024-03-10 11:41:10 +0530

    Add routing commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import React from 'react'
-import Header from './components/Header/Header'
-import Body from './components/Body/Body'
-import Footer from './components/Footer/Footer'
+import AppRouter from './Router/AppRouter'
 
 const App = () => {
   return (
-    <div className='w-full h-screen'> 
-        <Header/>
-        <Body/>
-        <Footer/>
-    </div>
+    <AppRouter />
   )
 }
 
