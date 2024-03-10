a28173a807ba8dfd2696ce0841520db71fdc7806
Ruwan <ruwanliyanagegalle1994@gmail.com>
2024-03-10 10:22:42 +0530

    Add dependencies to make the matirial effects

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import menu from '../../img/cart.svg';
-import cart from '../../img/menu.svg';
-import search from '../../img/search.svg';
+import MenuIcon from '@mui/icons-material/Menu';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import SearchIcon from '@mui/icons-material/Search';
+
 
 const Header = () => {
   return (
     <header className="fix top-0 left-0 z-[100] w-full p-5 bg-my flex items-center justify-between drop-shadow-header-shadow">
         {/* left side */}
         <div className='flex items-center'>
-            <img src={menu} alt="menu" className=' w-6 h-6 object-contain cursor-pointer'/>
+            <MenuIcon sx={{color: "white"}} />
             <h1 className='ml-2 text-sm font-semibold'> 
                 Smart <span className='text-white'>Plaza</span>
             </h1>
             <div className='overflow-hidden ml-2 flex items-center rounded-full  bg-white'>
                 <input type='text' placeholder='search' className='ml-1 outline-none p-2 font-semibold text-sm w-[200px] bg-inherit'/>
-                <img src={search} alt="search" className='ml-2 w-6 h-6 object-contain cursor-pointer'/>
+                <SearchIcon sx={{color: "grey"}}/>
             </div>
         </div> 
 
         {/* right side */}
-        <img src={cart} alt="cart" className=' w-6 h-6 object-contain cursor-pointer'/>
+        <AddShoppingCartIcon sx={{color:"white"}}/>
     </header>
   )
 }
