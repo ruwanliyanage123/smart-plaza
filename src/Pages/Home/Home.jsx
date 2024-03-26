384ea408e70c3f1d57a74d759f7e79d361fff381
Ruwan <ruwanliyanagegalle1994@gmail.com>
2024-03-26 06:20:17 +0530

    Minor modification

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,30 +1,13 @@
-import { useRef, useState } from 'react'
+import { useState } from 'react'
 import Ads from './Ads'
 import Product from './Product'
 import Loading from '../../components/Loading/Loading';
-import PaymentModal from '../../Modals/PaymentModal';
 import { IconButton } from '@mui/material';
-import db from '../../firebase/firebase';
-import { addDoc, collection } from 'firebase/firestore';
 import getDataFromCollection from '../../Utils/datafetch/getDataFromCollection';
-
 
 const Home = () => {
   const [loading, setLoading] = useState(false);
-  // const paymentModalRef = useRef()
   if(loading)return <Loading />
-
-  const addData = ()=>{
-    addDoc(collection(db, "users"), {
-      first: "Ada",
-      last: "Lovelace",
-      born: 1815
-    }).then((docRef) =>{
-      console.log("Document written with ID: ", docRef.id);
-    }).catch((error)=>{
-      console.log("Error Adding Document: ", error);
-    })
-  }
 
   return (
     <div className=' pl-6 pr-6 px-5 py-[100px] w-full h-screen overflow-y-scroll' >
