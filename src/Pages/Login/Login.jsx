891f62d849ef58facea5083d8fe6c4eddae087f9
Ruwan <ruwanliyanagegalle1994@gmail.com>
2024-03-31 12:37:53 +0530

    Add basic login credentials

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { collection, query, where, getDocs } from "firebase/firestore";
+import React, { useRef } from 'react'
+import { useDispatch } from 'react-redux'
+import db from "../../firebase/firebase";
+import { addUser } from "../../Store/ReduxSlice/userSlice";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+  const navigate = useNavigate();
+  const loginSubmit = ()=>{
+    const q = query(collection(db, "users"), where("username", "==", 'ruwan'));
+    getDocs(q).then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+          dispatch(addUser(doc.data()))
+      });
+      navigate('/admin');
+    })
+    .catch((error) => {
+      console.log("Error getting documents: ", error);
+    });
+  }
   return (
-    <div>Login</div>
+    <div>
+      <input type='text' ref={usernameRef} placeholder='userName' />
+      <input type='password' ref={passwordRef} placeholder='password' />
+      <button onClick={loginSubmit}>Login</button>
+    </div>
   )
 }
 
--- src/Router/AdminProtected.jsx
+++ src/Router/AdminProtected.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
+import { userSelector } from '../Store/ReduxSlice/userSlice';
 
 const AdminProtected = () => {
-    const admin = false;
-    if(admin){
-        return <Outlet />
+    const user = useSelector(userSelector);
+    if(user.role){
+        if(user.role == "admin"){
+            return <Outlet />
+        }else{
+            return <Navigate to='/404' />;
+        }
     }else{
-        return <Navigate to='/404' />;
+        return <Navigate to='404' />
     }
 };
 
