898a2407cad89239bdd9a80fe1a66551ef65924d
Ruwan <ruwanliyanagegalle1994@gmail.com>
2024-03-31 13:36:40 +0530

    Add basic logout method

--- src/Pages/Admin/Admin.jsx
+++ src/Pages/Admin/Admin.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import MainContainer from '../../Layout/MainContainer';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { removeUser } from "../../Store/ReduxSlice/userSlice";
 
 const Admin = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const logout = ()=>{
+    dispatch(removeUser());
+    navigate('/admin');
+  }
+
   return (
     <MainContainer>
-        Admin
+       <button onClick={() => logout()}>Logout</button>
     </MainContainer>
   )
 }
--- src/Store/ReduxSlice/userSlice.js
+++ src/Store/ReduxSlice/userSlice.js
@@ -14,7 +14,7 @@ const userSlice = createSlice({
             state.user = action.payload;
         },
         removeUser:(state = initialState, action)=>{
-            state.user = action.payload;
+            state.user = {};
         }
     }
 })
