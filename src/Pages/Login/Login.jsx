41430f8c29d3ffada6bbfc09c4adff7ffeee5cee
Ruwan <ruwanliyanagegalle1994@gmail.com>
2024-03-31 20:17:33 +0530

    Add sign up page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,84 +1,68 @@
-import { collection, query, where, getDocs } from "firebase/firestore";
-import React, { useEffect, useRef } from 'react'
-import { useDispatch } from 'react-redux'
-import db, { auth } from "../../firebase/firebase";
-import { addUser } from "../../Store/ReduxSlice/userSlice";
-import { useNavigate } from "react-router-dom";
-import { onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
-import { AltRoute } from "@mui/icons-material";
+import React, { useState } from 'react'
 
 const Login = () => {
-  const dispatch = useDispatch();
-  const usernameRef = useRef();
-  const passwordRef = useRef();
-  const navigate = useNavigate();
-
-  const loginSubmit = ()=>{
-    const q = query(collection(db, "users"), where("username", "==", 'ruwan'));
-    getDocs(q).then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-          dispatch(addUser(doc.data()))
-      });
-      navigate('/admin');
-    })
-    .catch((error) => {
-      console.log("Error getting documents: ", error);
-    });
-  }
-
-  useEffect(()=>{
-    const userCheckfun = () =>{
-       onAuthStateChanged(auth, (user)=>{
-         console.log("user logging", user);
-         if(user){
-             const uid = user.uid;
-             console.log(user);
-         }else{
-             console.log(`user error`); 
-         }
-       });
-    };
-    userCheckfun();
-    return () => userCheckfun(); 
-  },[]);
-
-  const login = () =>{
-    const email = usernameRef.current.value;
-    const password = passwordRef.current.value;
-    signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      console.log(user);
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    });
-  }
-  
-  const login3 = () =>{
-    const user = auth.currentUser;
-    if(user){
-       console.log(user);
-    }else{
-      console.log('no user');
-    }
-  }
-
+  const [select, setSelect] = useState('login');
   return (
     <div>
-      <input type='email' ref={usernameRef} placeholder='email' />
-      <input type='password' ref={passwordRef} placeholder='password' />
-      <button onClick={login}>Login</button>
-      <button onClick={()=>{
-          signOut(auth).then(()=>{
-            alert('user sign out')
-          }).catch((error)=>{
-            alert(error.message);
-          });
-      }}>Logout</button>
+      {
+         select === 'login'? <LoginComponent />: select === "register"? <RegisterComponent /> :null
+      }
+      {/** change loggin or register */}
+
+      <div className=' '>
+      {
+        select === 'login'?(<p>Don't have an account? <span className=' cursor-pointer' onClick={()=>{setSelect('register')}}>Sign Up</span></p>): select === "register"? (<p>Already have an account <span className=' cursor-pointer' onClick={()=>{setSelect('login')}}>Sign In</span></p>) :null
+      }
+      </div>
+    </div>
+  ) 
+}
+
+export default Login;
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+const LoginComponent = ()=>{
+  return(
+    <div>
+      <h1>Login</h1>
     </div>
   )
 }
 
-export default Login
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+const RegisterComponent = () => {
+  const registerHandle = (e) =>{
+    e.preventDefault();
+    console.log(e.target[0].value);
+  }
+  return(
+  <div>
+    Register
+    <form onSubmit={registerHandle}>
+      <div>
+        <input type='text' placeholder='your First Name' />
+        <input type='text' placeholder='your Last Name' />
+      </div>
+        <input type='text' placeholder='your email' />
+        <input type='password' placeholder='your password' />
+        <input type='password' placeholder='confirm password' />
+        <input type='text' placeholder='your phone number' />
+        <button type='submit' >Submit</button>
+    </form>
+  </div>
+  )
+}
