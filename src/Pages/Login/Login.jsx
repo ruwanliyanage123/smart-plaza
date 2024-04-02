7952d2d45712e335e49109c2ff1c26d01e20de74
Ruwan <ruwanliyanagegalle1994@gmail.com>
2024-04-02 06:32:05 +0530

    Modified login panel

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,10 @@
+import { IconButton } from '@mui/material';
 import React, { useState } from 'react'
 
 const Login = () => {
   const [select, setSelect] = useState('login');
   return (
-    <div>
+    <div className=' w-full h-screen flex flex-col items-center justify-center'>
       {
          select === 'login'? <LoginComponent />: select === "register"? <RegisterComponent /> :null
       }
@@ -31,9 +32,23 @@ export default Login;
  */
 
 const LoginComponent = ()=>{
+  const login = () => {}
   return(
-    <div>
-      <h1>Login</h1>
+    <div className=' w-[90%] p-5 shadow-lg flex flex-col'>
+      <h1 className=' text-3xl font-bold mb-7'>Login</h1>
+      <form onSubmit={login}>
+        <div className='border border-gray-400 rounded-[5px]'>
+          {/* <label className=' absolute top-[-12px] left-2 bg-white px-1 font-medium text-xs'>email</label> */}
+          <label className=' bg-white px-1 font-medium text-md'>email</label>
+          <input className=' outline-none w-full' type='text' name='email' placeholder='Enter your email'/>
+        </div>
+        <div className='border border-gray-400  rounded-[5px]'>
+          {/* <label className=' absolute top-[-12px] left-2 bg-white px-1 font-medium text-xs'>password</label> */}
+          <label className=' bg-white px-1 font-medium text-md'>password</label>
+          <input className=' outline-none  w-full h-full' type='password' name='password' placeholder='Enter your password'/>
+        </div>
+        <button className=' font-bold px-5 py-2 border-[2px] hover:bg-slate-200 border-[black] rounded' >Login</button>
+      </form>
     </div>
   )
 }
@@ -50,19 +65,29 @@ const RegisterComponent = () => {
     console.log(e.target[0].value);
   }
   return(
-  <div>
-    Register
+    <div className=' w-[90%] p-5 shadow-lg flex flex-col'>
+    <h1 className=' text-3xl font-bold mb-7'>Register</h1>
     <form onSubmit={registerHandle}>
       <div>
-        <input type='text' placeholder='your First Name' />
-        <input type='text' placeholder='your Last Name' />
-      </div>
-        <input type='text' placeholder='your email' />
-        <input type='password' placeholder='your password' />
-        <input type='password' placeholder='confirm password' />
-        <input type='text' placeholder='your phone number' />
-        <button type='submit' >Submit</button>
+        <input  className=' outline-none' type='text' placeholder='your First Name' />
+        <input  className=' outline-none' type='text' placeholder='your Last Name' />
+      </div> 
+        <input  className=' outline-none' type='text' placeholder='your email' />
+        <input  className=' outline-none' type='password' placeholder='your password' />
+        <input  className=' outline-none' type='password' placeholder='confirm password' />
+        <input  className=' outline-none' type='text' placeholder='your phone number' />
+        <div>
+           <button className=' font-bold px-5 py-2 border-[2px] hover:bg-slate-200 border-[black] rounded' type='submit' >Register</button>
+        </div>
     </form>
   </div>
   )
+}
+
+
+const LoginInputBox = ({inputType, placeholder})=>{
+  return <div>
+    <label></label>
+    <input type={inputType} placeholder={placeholder} />
+  </div>
 }
--- src/Router/AdminProtected.jsx
+++ src/Router/AdminProtected.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
 import { userSelector } from '../Store/ReduxSlice/userSlice';
+import NoUrl from '../Pages/404/NoUrl';
 
 const AdminProtected = () => {
     const user = useSelector(userSelector);
+    const [canView, setCanview] = useState(false);
     if(user.role){
         if(user.role == "admin"){
             return <Outlet />
@@ -14,6 +16,7 @@ const AdminProtected = () => {
     }else{
         return <Navigate to='404' />
     }
+    return canView ? <Outlet /> : <NoUrl />;
 };
 
 export default AdminProtected
