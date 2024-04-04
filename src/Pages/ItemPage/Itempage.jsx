313421328fb5384dfad42bdc1713ddf4029ef3db
Ruwan <ruwanliyanagegalle1994@gmail.com>
2024-04-04 23:08:02 +0530

    Add currency converter

--- src/Pages/ItemPage/Itempage.jsx
+++ src/Pages/ItemPage/Itempage.jsx
@@ -1,5 +1,6 @@
-import { IconButton } from '@mui/material';
+import { IconButton, Rating } from '@mui/material';
 import React, { useRef } from 'react'
+import { currencyDisplay } from '../../Utils/fetures/currencyDisplay';
 
 const itemImageArr = [
   'https://wallpaperaccess.com/full/2676.jpg',
@@ -43,14 +44,26 @@ const Itempage = () => {
           }      
       </div>
       <div className=' w-full h-[1px] mt-7 bg-slate-400'/>
-      {/* product details */}
-      <div>
-        <h3 className=' text-3xl'> 
-          <span className=' font-normal text-red-700'>-40%</span> Rs. 2500.00/-
-        </h3>
-        <p className=' font-semibold'>List Price: Rs. 5000/-</p>
-      </div>
-      {/* video 28 19.38 */}
+        <div>
+          <h3 className=' text-3xl'> 
+            <span className=' font-normal text-red-700'>-40%</span> Rs. {currencyDisplay(2500)}/-
+          </h3>
+          <p className=' font-semibold'>
+            List Price: Rs. {currencyDisplay(5000)}/-
+          </p>
+          <p className=' text-green-900 text-base'>In stock</p>
+          <Rating value={3.5} precision={0.1} readOnly />
+          <p className=' font-semibold text-gray-800 text-xs'>Smart Plazza</p>
+          <div className=' w-full h-[1px] bg-slate-400 my-3' />
+          <p>
+            Condition:
+            <span className=' fond-semibold'>Brand New</span>
+          </p>
+          <div className=' flex items-center gap-2'>
+            <p>Quantity:</p>
+            <input className=' px-3 py-1 w-[50px] border-2 border-black rounded-lg outline-none border-gray-300' />
+          </div>
+        </div>
     </div>
   );
 };
