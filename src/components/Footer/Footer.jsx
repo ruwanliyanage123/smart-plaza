180780dd1bc48075d6bffbc6711caffdd7195ca7
Ruwan <ruwanliyanagegalle1994@gmail.com>
2024-04-11 23:46:45 +0530

    Add phone number

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,10 +7,15 @@ const Footer = () => {
         <div className='footer-icon1'>one</div>
         <div className='footer-icon2'>two</div>
         <div className='footer-icon3'>
+           <div>
             GLARK Fashion <br/>
             Godawaththa, <br/>
             Panangala West,  <br/>
             Panangala
+           </div>
+           <div>
+            +94 77 230 85 19
+           </div>
         </div>
     </footer>
   )
